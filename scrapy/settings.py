43b2a483c74df634b730d2384ad792b346533b4e
deshraj <deshrajdry@gmail.com>
2015-02-11 01:17:55 +0530

    as

--- scrapy/settings.py
+++ scrapy/settings.py
@@ -79,11 +79,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_ROOT = 'static'
-STATIC_URL = '/css/'
+STATIC_ROOT = 'css'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'css'),
+    # os.path.join(BASE_DIR, 'css'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 
